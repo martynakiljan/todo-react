513e1bcc85417944869c42a58914b792742bdd44
Martyna Kiljan <kmartynak@icloud.com>
2024-03-13 20:34:31 +0100

    added light and dark mode

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import "./App.css";
 import TodoContainer from "./components/TodoContainer";
 import Container from "@mui/material/Container";
 import CompletedTaskContainer from "./components/CompletedTasksContainer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBrush } from "@fortawesome/free-solid-svg-icons";
+
 import Tooltip from "@mui/material/Tooltip";
 import { ThemeContext } from "./context/context.js";
 import { createTheme } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { Typography } from "@mui/material";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { DarkMode } from "@mui/icons-material";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [mode, setMode] = useState("light");
+
+  console.log(mode);
 
   const theme = createTheme({
     palette: {
@@ -45,12 +50,28 @@ function App() {
         <Container maxWidth="md">
           <h1 className="todo-title">TODO LIST</h1>
           <Tooltip title="you can change theme!">
-            <FontAwesomeIcon icon={faBrush} onClick={changeMode} />
+            <IconButton onClick={changeMode} color="inherit">
+              {
+                <Typography
+                  color={mode === "light" ? "black" : "primary"}
+                  sx={{ pr: 1 }}
+                >
+                  {mode === "light" ? "dark" : "light"} mode
+                </Typography>
+              }
+              {mode === "dark" ? (
+                <Brightness7Icon color="primary" />
+              ) : (
+                <DarkMode />
+              )}
+            </IconButton>
           </Tooltip>
           <TodoContainer tasks={tasks} setTasks={setTasks} />
         </Container>
         <Container maxWidth="sm">
-          <h2 className="todo-title todo-title__completed">COMPLETED TASKS</h2>
+          <h2 className="todo-title todo-title__completed">
+            COMPLETED TASKS :)
+          </h2>
           <CompletedTaskContainer tasks={tasks} />
         </Container>
       </div>
